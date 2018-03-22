1208f101c519d967dfa1bd655d58fb583e150a5d
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-03-21 22:08:19 -0300

    Criação de novos arquivos.

---
+++ app.tsx
@@ -0,0 +1,19 @@
+/**
+ * Arquivo: app.tsx
+ * Data: 21/03/2018
+ * Descrição: Arquivo responsável por executar o app usando o react e o ts
+ * Author: Glaucia Lemos
+ *
+ */
+
+import * as React from "react";
+import * as ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+    <div>
+        <h1>Olá! Sejam Bem-Vindos(as) ao meetup React Rio!!!</h1>
+    </div>,
+    
+    document.getElementById("root")
+);
